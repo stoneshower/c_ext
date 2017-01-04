1280f1cc6e5b190f2cb96faf0b3d93e43c007230
Takahashi, Shinya | Shinya | ECCO <shinya.takahashi@rakuten.com>
2017-01-04 16:34:54 +0900

    editted the files to show rad config url

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
-	console.log(response);
-	alert(response);
+// this is the background code...
+
+// listen for our browerAction to be clicked
+chrome.browserAction.onClicked.addListener(function (tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'inject.js'
+	});
 });
--- myscript.js
+++ myscript.js
@@ -1,1 +1,74 @@
-chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerText);
+// console.log('script is happening');
+// console.log(document.getElementsByClassName('riDescription')[0]);
+// console.log(document.getElementById('test').textContent);
+
+document.addEventListener('DOMContentLoaded', function(){
+	console.log(document.getElementById('test').textContent);
+
+}, false);
+
+console.log('**********');
+
+$(window).ready(function(){
+	console.log(document.getElementById('test').textContent);
+	console.log('window ready  is happening')
+	// var h1text = $('h1').text();
+	// var tempKey = $('.riItemTitle a');
+
+	// tempKey.css('border', '5px solid red');
+	// $('.riHinner').css('border', '5px solid red');
+
+	// console.log('========');
+	// console.log($('.riHinner').text());
+	// console.log(tempKey);
+	console.log('========');
+
+})
+$(document).ready(function(){
+	console.log('document ready  is happening');
+	var radTableConfigUrl = $('.rad-table-config').attr('url');
+	console.log(radTableConfigUrl);
+	var ajaxContent = $.ajax({
+		url: radTableConfigUrl,
+		dataType: 'json',
+		cache: false
+	}).done(function(data, textStatus){
+		console.log(data);
+		console.log(textStatus);
+	}).fail(function(xhr, textStatus, errorThrown){
+		console.log('fail');
+	});
+	// var h1text = $('h1').text();
+	// var tempKey = $('.riItemTitle a');
+
+	// tempKey.css('border', '5px solid red');
+	// $('.riHinner').css('border', '5px solid red');
+
+	// console.log('========');
+	// console.log($('.riHinner').text());
+	// console.log(tempKey);
+	console.log('========');
+})
+
+
+// $(function(){
+	// var url = chrome.extension.getURL('toolbar.html');
+	// var height = '35px';
+	// var iframe = "<iframe src='"+url+"' id='myOwnCustomFirstToolbar12345' style='height:"+height+"'></iframe>";
+
+	// $('body').append(iframe).css('margin-top', '100px');
+
+	// var h1text = $('h1').text();
+	// var tempKey = $('.riItemTitle a');
+
+	// tempKey.css('border', '5px solid red');
+	// $('.riHinner').css('border', '5px solid red');
+
+	// console.log('========');
+	// console.log($('.riHinner').text());
+	// console.log(tempKey);
+	// console.log('========');
+// });
+
+
+// http://localhost/sample_chrome_ex_file/event.rakuten.co.jp/sake/sakeset/
