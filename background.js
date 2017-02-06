6547f2eed13ec6cb15b2e890a8550d336d375a3c
Takahashi, Shinya | Shinya | ECCO <shinya.takahashi@rakuten.com>
2017-02-06 14:07:18 +0900

    added storage - tesing btwn background and content scrpit

--- background.js
+++ background.js
@@ -1,4 +1,29 @@
 var fire = false;
+/*using storage*/
+// default
+// localStorage.removeItem('showResult');
+// localStorage.setItem('showResult', 'on');
+/*using storage*/
+
+// var manifest = chrome.runtime.getManifest();
+// console.log(manifest.content_scripts[0].js);
+
+
+// chrome.storage.local.set(window.entity, function() {
+//     console.log('stored');
+// });
+
+
+var window.entity = {};
+window.entity.hoge = {
+    test: 'Takahashi'
+};
+
+chrome.storage.local.set(window.entity, function() {
+    console.log('stored');
+});
+
+
 
 chrome.browserAction.onClicked.addListener(
 	function(tab){
@@ -7,30 +32,86 @@ chrome.browserAction.onClicked.addListener(
 		if(fire === true){
 			chrome.browserAction.setIcon({path: "on.png"});
 			chrome.browserAction.setBadgeText({text: 'ON'});
+
+
+
+			/*using storage*/
+			// localStorage.setItem('showResult', 'on');
+			// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+			//     if (request.method == "getLocalStorage")
+			//       sendResponse({data: localStorage});
+			//     else
+			//       sendResponse({});
+			// });
+			/*using storage*/
+			// window.entity = {
+			//     col1: 'shinya'
+			// };
+			// chrome.storage.local.set(window.entity.hoge.col1, function() {
+			// 	console.log('yes');
+			// });
+
+			/*trying to modify(set) manifest json*/
+			// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+			// 	sendResponse({data: window.entity})
+			// });
+
+
+
+
 		} else {
 			chrome.browserAction.setIcon({path: "off.png"});
 			chrome.browserAction.setBadgeText({text: 'OFF'});
+
+			/*using storage*/
+			// localStorage.removeItem('showResult');
+			// localStorage.setItem('showResult', 'off');
+			// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+			//     if (request.method == "getLocalStorage")
+			//       sendResponse({data: localStorage});
+			//     else
+			//       sendResponse({});
+			// });
+			/*using storage*/
+
+			// chrome.storage.local.remove(window.entity.hoge.col1, function() {
+			//     console.log('removed');
+			// });
+			// window.entity.hoge = {
+			//     col1: 'off'
+			// };
+			// chrome.storage.local.set(window.entity, function() {
+
+			// });
+
+			// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+			// 	sendResponse({data: window.entity})
+			// });
+
 		}
 	}
 )
 
+// localStorage['lastId'] = 'Takahashi';
 
-chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
-	if(fire === true){
-	  	chrome.tabs.executeScript(tab.id, {
-			file: 'myscript.js',
-			runAt: 'document_end',
-			allFrames: true
-		})
-	}
-});
-
-// chrome.browserAction.onClicked.addListener(function(tab){
-// 	chrome.tabs.executeScript(tab.id, {
-// 		file: 'myscript.js'
-// 	})
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//     if (request.method == "getLocalStorage")
+//       sendResponse({data: localStorage});
+//     else
+//       sendResponse({});
 // });
 
+
+
+// chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+// 	if(fire === true){
+// 	  	chrome.tabs.executeScript(tab.id, {
+// 			file: 'myscript.js',
+// 			runAt: 'document_end',
+// 			allFrames: true
+// 		})
+// 	}
+// });
 
 
 
--- myscript.js
+++ myscript.js
@@ -5,121 +5,159 @@
 
  // alert('content_script is loaded - on!!');
 
-
-(function($) {
-    /***
-     * Validation for JSON Rasta: Checking the number of the data and template elements
-     **/
-
-    // alert('start');
-
-    // Get the DOM element which wraps Rasta element
-    var $radTable = $('.rad-table-existItemDisplay');
+chrome.storage.local.get(window.entity, function(items) {
+    console.log(items.hoge.test);
+});
 
 
-    // Get stringified HTML element
-    function stringifyElement($element) {
-        return $('<div>').append($element.clone()).html();
-    }
 
 
-    // Loop the each Rasta Element
-    $radTable.each(function(){
-        // Get all the texts inside $radTable and removes white space
-        var $selfText = stringifyElement($(this));
+    /*using storage*/
+    // chrome.runtime.sendMessage({method: "getLocalStorage", key: "status"}, function(response) {
+        // console.log(response.data);
+    // });
+
+    // console.log(sessionStorage['showResult']);
+
+    // if( sessionStorage['showResult'] === 'on' ){
+    /*/using storage*/
 
 
-        console.log($selfText);
+    /*trying to modify(set) manifest json*/
+    // var manifest = chrome.runtime.getManifest();
+    // manifest.content_scripts[0].js = [""];
+    // manifest.content_scripts[0].js = ["jquery.js", "dummy.js"];
+    // chrome.runtime.sendMessage({method: "getLocalStorage", key: "status"}, function(response) {
+    //     console.log(response.data);
+    // });
+    /*/trying to modify(set) manifest json*/
 
-        // Remove white space
-        var $selfText = $selfText.replace(/\s/g, '');
+        (function($) {
+            /***
+             * Validation for JSON Rasta: Checking the number of the data and template elements
+             **/
 
-        // Get the array of the texts which match "ALPHABET" + "NUMBER" + "ALPHABET" + "NUMBER" or "ALPHABET + NUMBER";
-        $selfText = $selfText.match(/#?[(A-Z)]+[(0-9)]+[(A-Z)]+[(0-9)]#?|#?[(A-Z)]+[(0-9)]#?/g);
+            // alert('start');
+
+            // Get the DOM element which wraps Rasta element
+            var $radTable = $('.rad-table-existItemDisplay');
 
 
-        // Remove the duplicated array
-        // $selfText = $selfText.filter(function (element, index, array) {
-        //     return array.indexOf(element) === index;
+            // Get stringified HTML element
+            function stringifyElement($element) {
+                return $('<div>').append($element.clone()).html();
+            }
+
+
+            // Loop the each Rasta Element
+            $radTable.each(function(){
+                // Get all the texts inside $radTable and removes white space
+                var $selfText = stringifyElement($(this));
+
+
+                // console.log($selfText);
+
+                // Remove white space
+                var $selfText = $selfText.replace(/\s/g, '');
+
+                // Get the array of the texts which match "ALPHABET" + "NUMBER" + "ALPHABET" + "NUMBER" or "ALPHABET + NUMBER";
+                $selfText = $selfText.match(/#?[(A-Z)]+[(0-9)]+[(A-Z)]+[(0-9)]#?|#?[(A-Z)]+[(0-9)]#?/g);
+
+
+                // Remove the duplicated array
+                // $selfText = $selfText.filter(function (element, index, array) {
+                //     return array.indexOf(element) === index;
+                // });
+                $selfText = [...new Set($selfText)];
+
+
+                // console.log($selfText);
+
+                // Set default number to count as zero
+                var rastaTempImageNum = 0;
+                var rastaTempTextNum = 0;
+                var rastaTempShopNum = 0;
+                var rastaTempShopIcomNum = 0;
+                var rastaTempIconNum = 0;
+
+                // Iterate the number of the each template element
+                $.each( $selfText, function(key, value){
+                    if( /^#IMAGE[0-9]+/g.test(value) ){ rastaTempImageNum++; }
+                    if( /^#TEXT[0-9]+/g.test(value) ){ rastaTempTextNum++; }
+                    if( /^#SHOP[0-9]+/g.test(value) ){ rastaTempShopNum++; }
+                    if( /^#ICON[0-9]+/g.test(value) ){ rastaTempIconNum++; }
+                });
+
+                // Get the DOM config element
+                var $rastaConfig = $(this).find('.rad-table-config');
+
+                // Get the value of the each attribute of config
+                var rastaImageConfigNum = parseInt( $rastaConfig.attr('imagenum') );
+                var rastaTextConfigNum = parseInt( $rastaConfig.attr('textnum') );
+                var rastaShopConfigNum = parseInt( $rastaConfig.attr('shopnum') );
+                var rastaIconConfigNum = parseInt( $rastaConfig.attr('iconnum') );
+
+
+
+            // console.log('image: ' + rastaTempImageNum + ' : ' + rastaImageConfigNum);
+            // console.log('text: ' + rastaTempTextNum + ' : ' + rastaTextConfigNum);
+            // console.log('shop: ' + rastaTempShopNum + ' : ' + rastaShopConfigNum);
+            // console.log('iconnum: ' + rastaTempIconNum + ' : ' + rastaIconConfigNum);
+
+
+                // Compare the number of the data and template elements
+                if(rastaTempImageNum === rastaImageConfigNum){
+                    var rastaImageNote = '<p>IMAGE NUM IS FINE</p>';
+                }else{
+                    var rastaImageNote ='<p class="rastaNoteError">IMAGE NUM IS WRONG!!!</p>';
+                }
+
+                if(rastaTempTextNum === rastaTextConfigNum){
+                    var rastaTextNote = '<p>TEXT NUM IS FINE</p>';
+                }else{
+                    var rastaTextNote = '<p class="rastaNoteError">TEXT NUM IS WRONG!!!</p>';
+                }
+
+                if(rastaTempShopNum === rastaShopConfigNum){
+                    var rastaShopNote = '<p>SHOP NUM IS FINE</p>';
+                }else{
+                    var rastaShopNote = '<p class="rastaNoteError">SHOP NUM IS WRONG!!!</p>';
+                }
+
+                if(rastaTempIconNum === rastaIconConfigNum){
+                    var rastaIconNote = '<p>ICON NUM IS FINE</p>';
+                }else{
+                    var rastaIconNote = '<p class="rastaNoteError">ICON NUM IS WRONG!!!</p>';
+                }
+
+                // Output the result on DOM
+                $( '<style>.rastaNote { border: 1px solid #4E4E4E;padding: 5px;box-sizing: border-box; }.rastaNoteError{ color:red;font-weight:bold; }</style>' ).appendTo( 'head' );
+                $(this).prepend('<div class="rastaNote" />');
+                    $(this).find('.rastaNote').prepend(
+                        rastaImageNote +
+                        rastaTextNote +
+                        rastaShopNote +
+                        rastaIconNote
+                    );
+                // Show $('.rad-table-existItemDisplay');
+                $radTable.show();
+            });
+
+            // alert('end');
+
+        })(jQuery);
+// }
+        /*using storage*/
+        //   } else{
+        //     console.log(response.data);
+        //   }
+
         // });
-        $selfText = [...new Set($selfText)];
-
-
-        // console.log($selfText);
-
-        // Set default number to count as zero
-        var rastaTempImageNum = 0;
-        var rastaTempTextNum = 0;
-        var rastaTempShopNum = 0;
-        var rastaTempShopIcomNum = 0;
-        var rastaTempIconNum = 0;
-
-        // Iterate the number of the each template element
-        $.each( $selfText, function(key, value){
-            if( /^#IMAGE[0-9]+/g.test(value) ){ rastaTempImageNum++; }
-            if( /^#TEXT[0-9]+/g.test(value) ){ rastaTempTextNum++; }
-            if( /^#SHOP[0-9]+/g.test(value) ){ rastaTempShopNum++; }
-            if( /^#ICON[0-9]+/g.test(value) ){ rastaTempIconNum++; }
-        });
-
-        // Get the DOM config element
-        var $rastaConfig = $(this).find('.rad-table-config');
-
-        // Get the value of the each attribute of config
-        var rastaImageConfigNum = parseInt( $rastaConfig.attr('imagenum') );
-        var rastaTextConfigNum = parseInt( $rastaConfig.attr('textnum') );
-        var rastaShopConfigNum = parseInt( $rastaConfig.attr('shopnum') );
-        var rastaIconConfigNum = parseInt( $rastaConfig.attr('iconnum') );
+        /*using storage*/
 
 
 
-    console.log('image: ' + rastaTempImageNum + ' : ' + rastaImageConfigNum);
-    console.log('text: ' + rastaTempTextNum + ' : ' + rastaTextConfigNum);
-    console.log('shop: ' + rastaTempShopNum + ' : ' + rastaShopConfigNum);
-    console.log('iconnum: ' + rastaTempIconNum + ' : ' + rastaIconConfigNum);
 
-
-        // Compare the number of the data and template elements
-        if(rastaTempImageNum === rastaImageConfigNum){
-            var rastaImageNote = '<p>IMAGE NUM IS FINE</p>';
-        }else{
-            var rastaImageNote ='<p class="rastaNoteError">IMAGE NUM IS WRONG!!!</p>';
-        }
-
-        if(rastaTempTextNum === rastaTextConfigNum){
-            var rastaTextNote = '<p>TEXT NUM IS FINE</p>';
-        }else{
-            var rastaTextNote = '<p class="rastaNoteError">TEXT NUM IS WRONG!!!</p>';
-        }
-
-        if(rastaTempShopNum === rastaShopConfigNum){
-            var rastaShopNote = '<p>SHOP NUM IS FINE</p>';
-        }else{
-            var rastaShopNote = '<p class="rastaNoteError">SHOP NUM IS WRONG!!!</p>';
-        }
-
-        if(rastaTempIconNum === rastaIconConfigNum){
-            var rastaIconNote = '<p>ICON NUM IS FINE</p>';
-        }else{
-            var rastaIconNote = '<p class="rastaNoteError">ICON NUM IS WRONG!!!</p>';
-        }
-
-        // Output the result on DOM
-        $( '<style>.rastaNote { border: 1px solid #4E4E4E;padding: 5px;box-sizing: border-box; }.rastaNoteError{ color:red;font-weight:bold; }</style>' ).appendTo( 'head' );
-        $(this).prepend('<div class="rastaNote" />');
-            $(this).find('.rastaNote').prepend(
-                rastaImageNote +
-                rastaTextNote +
-                rastaShopNote +
-                rastaIconNote
-            );
-        // Show $('.rad-table-existItemDisplay');
-        $radTable.show();
-    });
-
-    // alert('end');
-
-})(jQuery);
 
 
 
